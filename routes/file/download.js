ed052466113e219dd0028bda4b92f1a0f990e7d9
Yuk Fang <fangyu.k@bytedance.com>
2025-09-10 19:45:14 +0800

    impl corp_id

--- routes/file/download.js
+++ routes/file/download.js
@@ -4,24 +4,30 @@ const path = require('path');
 
 const router = new Router({ prefix: `/file/download`});
 
-router.get('/:key', async (ctx) => {
-    const fileName = ctx.params.key.toLowerCase();
-    const filePath = path.join(__dirname, 'files', fileName);
+router.get('/:corp_id/:report_name', async (ctx) => {
+    const corpId = ctx.params.corp_id;
+    const reportName = ctx.params.report_name;
+    console.log('corpId', corpId);
+    console.log('reportName', reportName);
+
+    // const reportDir = path.join(__dirname, 'reports', corpId);
+    const reportPath = path.join(__dirname,   'reports', corpId, reportName);
+    console.log('reportPath', reportPath);
 
     try {
-        const stats = fs.statSync(filePath);
+        const stats = fs.statSync(reportPath);
         if (stats.isFile()) {
-            ctx.attachment(fileName);
-            ctx.body = fs.createReadStream(filePath);
+            ctx.attachment(reportPath);
+            ctx.body = fs.createReadStream(reportPath);
         } else {
             ctx.status = 404;
-            ctx.body = { error: `File not found: ${fileName}` };
+            ctx.body = { error: `Report not found: ${reportName}` };
         }
     } catch (error) {
         console.error(error);
         if (error.code === 'ENOENT') {
             ctx.status = 404;
-            ctx.body = { error: `File not found: ${fileName}` };
+            ctx.body = { error: `Report not found: ${reportName}` };
         } else {
             ctx.status = 500;
             ctx.body = { error: 'Internal Server Error' };
