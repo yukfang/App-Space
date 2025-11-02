e2e908b18ebc71f52208d9ae6f92f77140d92b80
Yuk Fang <yukfang@Mac.lan>
2025-11-02 22:54:00 +0800

    add login key

--- routes/login/index.js
+++ routes/login/index.js
@@ -1,16 +1,19 @@
 const Router = require('koa-router');
+const fs = require('fs')
 const router = new Router({ prefix: '/login' });
+
+const key = fs.readFileSync('./id_proxyuser.key.txt', 'utf-8')
 
 // 模拟数据库用户表
 const USERS = {
     "user1@123" : {
         name: "user1",
-        dstAddr: '120.55.244.123',
+        dstAddr: 'aws.my.002.qcreator.tech',
         dstPort: 22,
-        sshUserName: 'root',
-        privateKey: '11111',
+        sshUserName: 'proxyuser',
+        privateKey: key,
         localPort: 7777,
-        defaultPageUrl: "https://www.qcreator.tech/user1.html",
+        defaultPageUrl: "https://www.qcreator.tech/",
         profiles: [
             {name: "Shopee Shop 1"}, 
             {name: "Shopee Shop 2"}, 
@@ -20,9 +23,10 @@ const USERS = {
         name: "user2",
         dstAddr: '120.55.244.123',
         dstPort: 22,
-        sshUserName: 'root',
+        sshUserName: 'proxyuser',
+        privateKey: key,
         localPort: 7777,
-        defaultPageUrl: "https://www.qcreator.tech/user2.html",
+        defaultPageUrl: "https://www.qcreator.tech/",
         profiles: [
             {name: "Shopee Shop 3"}, 
         ]
